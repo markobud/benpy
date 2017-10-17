2d1084115e41f4d97d422e676afba7a85ebff1d8
vintache-d <damien.vintache@univ-nantes.fr>
2017-10-17 17:06:58 +0200

    add include dir to compile bensolve-mod

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
 import numpy
+import os
+
 
 ext = Extension("benpy",sources=["benpy.pyx",
                                     "bensolve-mod/bslv_main.c",
@@ -10,12 +12,12 @@ ext = Extension("benpy",sources=["benpy.pyx",
                                     "bensolve-mod/bslv_poly.c",
                                     "bensolve-mod/bslv_lp.c"
                                     ],
+                        include_dirs=[numpy.get_include(), os.environ['CONDA_PREFIX'] + "/include"],
                         libraries=['glpk','m'],
 			extra_compile_args=['-std=c99','-O3']
                                     )
 setup(
 	name="benpy",
-	ext_modules = cythonize([ext]),
-	include_dirs=[numpy.get_include()]
+	ext_modules = cythonize([ext])
 )
 
