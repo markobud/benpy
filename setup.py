e169e312b2b409dd7d2edc9edc9011e8aa700329
vintache-d <damien.vintache@univ-nantes.fr>
2017-11-14 16:15:20 +0100

    switch to setuptools

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy
 import os
@@ -12,12 +12,20 @@ ext = Extension("benpy",sources=["benpy.pyx",
                                     "bensolve-mod/bslv_poly.c",
                                     "bensolve-mod/bslv_lp.c"
                                     ],
-                        include_dirs=[numpy.get_include(), os.environ['CONDA_PREFIX'] + "/include"],
-                        libraries=['glpk','m'],
-			extra_compile_args=['-std=c99','-O3']
-                                    )
+                include_dirs=[numpy.get_include(), os.environ['CONDA_PREFIX'] + "/include"],
+                libraries=['glpk','m'],
+			    extra_compile_args=['-std=c99','-O3']
+               )
+
 setup(
 	name="benpy",
+    version='1.0',
+    description='Python Benpy Utility',
+    author='Marko Budinich',
+    author_email='marko.budinich@ls2n.fr',
+    url='https://gitlab.univ-nantes.fr/mbudinich/benpy',
+    license='GPLv3',
+    long_description='Benpy',
+    platforms='linux, OSX',
 	ext_modules = cythonize([ext])
 )
-
