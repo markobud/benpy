eed671b40e7b690f540938707c3de6775e290494
vintache-d <damien.vintache@univ-nantes.fr>
2017-11-17 11:06:43 +0100

    add option alg_phase2 and change the input filename for problem2

--- src/tests/test_benpy.py
+++ src/tests/test_benpy.py
@@ -2,10 +2,10 @@ import benpy
 
 problem = benpy.vlpProblem()
 problem.filename = "ex01.vlp"
-problem.options = {'message_level': 3}
+problem.options = {'message_level': 3, 'alg_phase2': 'dual', 'solution': True}
 sol = benpy.solve(problem)
 
 problem2 = benpy.vlpProblem()
-problem2.filename = "ex01copy.vlp"
+problem2.filename = "ex02.vlp"
 problem2.options = problem.options
 sol2 = benpy.solve(problem2)
